fd1812caed9a5ef70c0709e2697d214ec6a9d95d
shraddha.devaiya.1998@gmail.com <shraddha.devaiya.1998@gmail.com>
2023-12-02 16:34:19 -0800

    add ot logic for labeling

--- controllers/work_OTFController.py
+++ controllers/work_OTFController.py
@@ -6,7 +6,6 @@ __copyright__ = 'Copyright 2020, The Bug Report Summarization Project @ Sybil-La
 __license__ = 'MIT'
 __maintainer__ = 'Akalanka Galappaththi'
 
-import pandas as pd
 import os
 import csv
 import pickle
@@ -14,58 +13,43 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 from controllers.TextCleanController import TextCleanController
 
-
-
 class OTFController:
     """Tag off-topic comments as OT.
     """
     
     def __init__(self):
-        self.data1 = pd.read_csv("/home/shraddha/sh_work/TOBE/data/9_17_human.csv", engine="python")
-        self.i = 0
         curr_dir = os.getcwd()
         #model_path = os.path.join(curr_dir, 'data', 'yt_git_tfidf_naive.sav')
         #tfidfvect_path = os.path.join(curr_dir, 'data', 'yt_git_tfidf.sav')
-        '''
-        model_path = os.path.join(curr_dir, 'data', 'man_try1_ot_naive.sav')
-        tfidfvect_path = os.path.join(curr_dir, 'data', 'man_try1_ot_tfidf.sav')
+        model_path = os.path.join(curr_dir, 'data', 'yt_git_model.sav')
+        tfidfvect_path = os.path.join(curr_dir, 'data', 'tfidf.sav')
         
         with open(model_path, 'rb') as file:
             self.svm_model = pickle.load(file)
             
         with open(tfidfvect_path, 'rb') as file:
             self.tfidf_vect = pickle.load(file)
-        '''
     
-    def findOTComment(self, sentence, count):
+    def findOTComment(self, sentence):
         """Add a tag OT to the sentence if it is classified as off-topic
         Parameters
         ----------
         sentence : object
             Sentence object
         """
-        #count = count + 1
-        #self.i = self.i + 1
-        #print("Sentence no. ",self.i)
-        print("Sentence no. ",count)
         
+            
         tcc = TextCleanController()
         sentence.set_cleaned_text(tcc.clean_sentence(sentence.get_text()))
         
         #Since transform expects a iterable object make sure to change str to a list
         input_list = list()
         input_list.append(sentence.get_text())
-        '''
         
         sent_tfidf = self.tfidf_vect.transform(input_list)
         
-        pred = self.svm_model.predict(sent_tfidf) 
-        '''
-        if self.data1['FINAL_OT'][count] == 1:
-            print("SHDBG: OT label no. ", count)
-            if 'OT' not in sentence.get_tags():
+        pred = self.svm_model.predict(sent_tfidf)
+        
+        if pred[0] == 0:
+            if 'OT' not in sentence.get_tags() and 'Code' not in sentence.get_tags():
                 sentence.add_a_tag('OT')
-    
-        #if pred[0] == 1:
-        #    if 'OT' not in sentence.get_tags() and 'Code' not in sentence.get_tags():
-        #        sentence.add_a_tag('OT')
--- work_app.py
+++ work_app.py
@@ -13,14 +13,13 @@ import json
 from jinja2 import Template
 
 from controllers.BugReportController import BugReportController
-from controllers.OTFController import OTFController
+#from controllers.OTFController import OTFController
 from controllers.RSFController import RSFController
 from controllers.URLController import URLController
 from controllers.CWController import CWController
 # from controllers.CFGController import CFGController
-# from controllers.SyntaxController import SyntaxController
-#SH_FIX
-#from controllers.TopicModelController import TopicModelController
+#from controllers.SyntaxController import SyntaxController
+from controllers.TopicModelController import TopicModelController
 from controllers.DescriptionController import DescriptionController
 from controllers.TextCleanController import TextCleanController
 from view.Display import Display
@@ -40,28 +39,22 @@ def display():
     
     # list index selector
     i = 0
-    #SH_FIX
-    count = 0
     
     # create controller objects
-    otc = OTFController()
+    #otc = OTFController()
     rsc = RSFController()
     urlc = URLController()
     # cfgc = CFGController()
-    # syc = SyntaxController()
-    #tmc = TopicModelController()
+    #syc = SyntaxController()
+    tmc = TopicModelController()
     tcc = TextCleanController()
-    
     
     for turn in list_of_bugs[i].get_turns():
         for sent in turn.get_sentences():
             # cfgc.setSentenceType(sent)
             urlc.findURL(sent)
             #syc.findSyntax(sent)
-            #SH_FIX
             #otc.findOTComment(sent)
-            otc.findOTComment(sent, count)
-            count = count + 1
             
             
             
@@ -76,10 +69,9 @@ def display():
     cwc = CWController()
     cwc.findLinks(list_of_bugs[i]) 
     
-    #SH_FIX
     # find topic words in sentences
-    #topics = tmc.extractTopics(list_of_bugs[i])
-    #tmc.findTopicWordMatches(list_of_bugs[i], topics)
+    topics = tmc.extractTopics(list_of_bugs[i])
+    tmc.findTopicWordMatches(list_of_bugs[i], topics)
     
     # find steps to reproduce, expected results and actual results in the first comment
     dc = DescriptionController()
@@ -88,7 +80,7 @@ def display():
         if turn.get_id() == 1:
             dc.findBugDescription(turn, list_of_bugs[i].get_title())
             break      
-    #SH_FIX
+    
     # run all sentences through TextCleanController to make a better display of sentences
     for turn in list_of_bugs[i].get_turns():
         for sent in turn.get_sentences():
@@ -109,115 +101,26 @@ def selectDisplay():
     # create a ListOfBugReport object and get the list
     bugs = BugReportController().createBugReport()
     list_of_bugs = bugs.get_list_of_bugs()
-    #SH_FIX
-    count = 0
+    
     # list index selector
     i = int(request.args.get('id'))
     # print(i)
     
     # create controller objects
-    otc = OTFController()
+    #otc = OTFController()
     rsc = RSFController()
     urlc = URLController()
     # cfgc = CFGController()
-    # syc = SyntaxController()
-    #tmc = TopicModelController()
+    #syc = SyntaxController()
+    tmc = TopicModelController()
     tcc = TextCleanController()
-
-    if i == 0:
-        count = 0
-    elif i == 1:
-        count = 31
-    elif i == 2:
-        count = 31 + 57
-    elif i == 3:
-        count = 31 + 57 + 91
-    elif i == 4:
-        count = 31 + 57 + 91 + 44
-    elif i == 5:
-        count = 31 + 57 + 91 + 44 + 102
-    elif i == 6:
-        #FIXME
-        count = 31 + 57 + 91 + 44 + 102 + 31
-    elif i == 7:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56
-    elif i == 8:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82
-    elif i == 9:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17
-    elif i == 10:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50
-    elif i == 11:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41
-    elif i == 12:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73
-    elif i == 13:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50
-    elif i == 14:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52
-    elif i == 15:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98
-    elif i == 16:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69
-    elif i == 17:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85
-    elif i == 18:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46
-    elif i == 19:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56
-    elif i == 20:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46
-    elif i == 21:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18
-    elif i == 22:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194
-    elif i == 23:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65
-    elif i == 24:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90
-    elif i == 25:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90 + 60
-    elif i == 26:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90 + 60 + 136
-    elif i == 27:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90 + 60 + 136 + 55
-    elif i == 28:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90 + 60 + 136 + 55 + 24
-    elif i == 29:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90 + 60 + 136 + 55 + 24 + 93
-    elif i == 30:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90 + 60 + 136 + 55 + 24 + 93 + 50
-    elif i == 31:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90 + 60 + 136 + 55 + 24 + 93 + 50 + 39
-    elif i == 32:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90 + 60 + 136 + 55 + 24 + 93 + 50 + 39 + 85
-    elif i == 33:
-        count = 31 + 57 + 91 + 44 + 102 + 31 + 56 + 82 + 17 + 50 + 41 + 73 + 50 + 52 + 98 + 69 + 85 + 46 \
-            + 56 + 46 + 18 + 194 + 65 + 90 + 60 + 136 + 55 + 24 + 93 + 50 + 39 + 85 + 104
 
     for turn in list_of_bugs[i].get_turns():
         for sent in turn.get_sentences():
             # cfgc.setSentenceType(sent)
             urlc.findURL(sent)
-            # syc.findSyntax(sent)
-            #SH_FIX
+            #syc.findSyntax(sent)
             #otc.findOTComment(sent)
-            otc.findOTComment(sent, count)
-            count = count + 1
             
             
             
@@ -232,10 +135,9 @@ def selectDisplay():
     cwc = CWController()
     cwc.findLinks(list_of_bugs[i]) 
     
-    #SH_FIX
     # find topic words in sentences
-    #topics = tmc.extractTopics(list_of_bugs[i])
-    #tmc.findTopicWordMatches(list_of_bugs[i], topics)
+    topics = tmc.extractTopics(list_of_bugs[i])
+    tmc.findTopicWordMatches(list_of_bugs[i], topics)
     
     # find steps to reproduce, expected results and actual results in the first comment
     dc = DescriptionController()
